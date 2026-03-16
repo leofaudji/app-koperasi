2ea8d85440c045ab1705a7eae252404b4d955f2a
devops <devops@assistindo.com>
2026-03-16 13:21:49 +0700

    edit data debug

--- verify_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'api/config/database.php';
-$db = Database::getInstance();
-$tables = $db->fetchAll("SHOW TABLES");
-foreach ($tables as $t) {
-    echo array_values($t)[0] . "\n";
-}
